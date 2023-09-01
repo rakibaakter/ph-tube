f4908224e664ee2362d19264f9431b7c27cd2fa9
rakibaakter <rakibaakter635@gmail.com>
2023-09-01 22:13:34 +0600

    fetch api of category id

--- js/script.js
+++ js/script.js
@@ -5,19 +5,27 @@
 const categoriesTypeHandler =async() =>{
     const res = await fetch('https://openapi.programming-hero.com/api/videos/categories')
     const categoriesList = await res.json();
-    const categoriesTypeContainer = document.getElementById('categories-type-container');
-    console.log(categoriesList.data)
     const categories = categoriesList.data;
+
+    displayCategoriesTitle(categories);
+}
+const displayCategoriesTitle = (categories) =>{
+    const categoriesTypeContainer = document.getElementById('categories-type-container');
     categories.forEach(category=> {
         const listTab = document.createElement('a');
         // listTab.classList = "tab";
         listTab.innerHTML = `
-        <button class="btn btn-active btn-ghost text-black px-6">${category.category}</button></a> 
+        <button onclick = "showCard('${category.category_id}')" class="btn btn-active btn-ghost text-black px-6">${category.category}</button></a> 
         `
-
         categoriesTypeContainer.appendChild(listTab);
     });
-
 }
-categoriesTypeHandler()
+
+const showCard = async(id ='1000') =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
+    const data = await res.json();
+    console.log(data.data);
+}
+categoriesTypeHandler();
+showCard()
 
